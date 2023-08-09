bf6ee494f2d6d491d9dfecc3fcaeba832e8f6faf
DaFlusher <joyibe5@gmail.com>
2023-08-09 23:40:43 +0100

    Updating the state of the BookingForm, preventinf default form behaviour

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -3,8 +3,9 @@ const BookingForm = () => {
 
     
     const [date, setDate]= useState(new Date().toLocaleDateString());
-    const[availabletime, setAvailableTime]= useState('');
+    const[availableTime, setAvailableTime]= useState('');
     const[number, setNumber]= useState(1);
+    const[occassion, setOccassion]= useState('')
 
     const handleDateChange=(e)=>{
         setDate(e.target.value)
@@ -18,14 +19,25 @@ const BookingForm = () => {
         setNumber(Number(e.target.value))
     }
     
+    const handleOccassionChange=(e)=>{
+        setOccassion(e.target.value)
+    }
 
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setAvailableTime('');
+        setDate('');
+        setNumber('');
+        setOccassion('')
+    }
     return ( 
         <>
-            <form style={{ display: "grid", maxWidth: 200, gap: 20 }}>
+            <form  onSubmit={handleSubmit} style={{ display: "grid", maxWidth: 200, gap: 20 }}>
                             <label htmlFor="res-date">Choose date</label>
                             <input type="date" id="res-date" />
+
                             <label htmlFor="res-time">Choose time</label>
-                            <select id="res-time ">
+                            <select id="res-time " value={availableTime} onChange={handleTimeChange}>
                                 <option>17:00</option>
                                 <option>18:00</option>
                                 <option>19:00</option>
@@ -33,13 +45,15 @@ const BookingForm = () => {
                                 <option>21:00</option>
                                 <option>22:00</option>
                             </select>
+                            <p>You chose:{availableTime}</p>
                             <label htmlFor="guests">Number of guests</label>
                             <input type="number" placeholder={1} min={1} max={10} id="guests" />
                             <label htmlFor="occasion">Occasion</label>
-                            <select id="occasion">
-                                <option>Birthday</option>
-                                <option>Anniversary</option>
+                            <select id="occasion" value={occassion} onChange={handleOccassionChange}>
+                                <option value='birthday'>Birthday</option>
+                                <option value='anniversary'>Anniversary</option>
                             </select>
+                            <p>You chose:{occassion}</p>
                             <input type="submit" defaultValue="Make Your reservation" />
                         </form>
         </>
