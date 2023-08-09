1a246cf04f0bf7d90f1d18162eea8d5aef739d4c
DaFlusher <joyibe5@gmail.com>
2023-08-09 23:22:02 +0100

    Creating the BookingForm and adding the useState within it

--- src/ReserveTable.js
+++ src/ReserveTable.js
@@ -4,26 +4,11 @@ import Header from './Header';
 import Nav from './navigation/Nav';
 import { useState } from 'react';
 import Footer from './Footer';
+import BookingForm from './BookingForm';
 
 
 
 const ReserveTable = () => {
-    
-    const [date, setDate]= useState(new Date().toLocaleDateString());
-    const[availabletime, setAvailableTime]= useState('');
-    const[number, setNumber]= useState(1);
-
-    const handleDateChange=(e)=>{
-        setDate(e.target.value)
-    }
-
-    const handleTimeChange=(e)=>{
-        setAvailableTime(e.target.value)
-    }
-
-    const handleNumberChange=(e)=>{
-        setNumber(Number(e.target.value))
-    }
     
     return (
             
@@ -41,28 +26,7 @@ const ReserveTable = () => {
                     <h2 className='table-subhead'>Please enter your dining info below</h2>
                     
                     <section>
-                        <form style={{ display: "grid", maxWidth: 200, gap: 20 }}>
-                            <label htmlFor="res-date">Choose date</label>
-                            <input type="date" id="res-date" />
-                            <label htmlFor="res-time">Choose time</label>
-                            <select id="res-time ">
-                                <option>17:00</option>
-                                <option>18:00</option>
-                                <option>19:00</option>
-                                <option>20:00</option>
-                                <option>21:00</option>
-                                <option>22:00</option>
-                            </select>
-                            <label htmlFor="guests">Number of guests</label>
-                            <input type="number" placeholder={1} min={1} max={10} id="guests" />
-                            <label htmlFor="occasion">Occasion</label>
-                            <select id="occasion">
-                                <option>Birthday</option>
-                                <option>Anniversary</option>
-                            </select>
-                            <input type="submit" defaultValue="Make Your reservation" />
-                        </form>
-
+                        <BookingForm/>
                     </section>
 
                     <p><a href='#' style={{marginRight:'4%', fontSize:'18px'}}>Next Page</a>Pg 1 of 2</p>
